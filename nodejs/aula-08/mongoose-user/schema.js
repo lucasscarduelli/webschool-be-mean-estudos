92614b160af8f28f7d213f71f977acf23fb6f2e7
Lucas Boeing Scarduelli <lucas@scarduelli.com>
2016-08-03 11:21:15 -0300

    Aula 08

--- nodejs/aula-08/mongoose-user/schema.js
+++ nodejs/aula-08/mongoose-user/schema.js
@@ -5,31 +5,14 @@ const Schema = mongoose.Schema;
 
 mongoose.connect('mongodb://localhost/mongoose-user-test');
 
-const userSchema = new Schema({
+const _schema = {
     name : require('./fields/fields-name'),
     password : require('./fields/fields-password'),
     email : require('./fields/fields-email'),
     type : require('./fields/fields-type'),
     created_at : require('./fields/fields-created_at')
-});
-
-const _userData = {
-    name : 'Lucas Boeing Scarduelli',
-    password : 'abc1234',
-    email: 'scarduelli.com'
 };
 
-const User = mongoose.model('User', userSchema);
+const userSchema = new Schema(_schema);
 
-//User.create(_userData, (err, data) => {
-//    if (err) return console.log('Erro: ', err);
-//    return console.log('Inseriu: ', data);
-//});
-//
-//User.findOne({}, (err, data) => {
-//    if (err) return console.log('Erro: ', err);
-//    return console.log('Achou: ', data.name);
-//});
-
-const u = new User(_userData);
-console.log(u.validateSync());
+module.exports = userSchema;
